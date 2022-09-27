89fbae03eaa9e4dc1061a6b732dd6b702b9d2683
Truong Pham <thanhtruong27701@gmail.com>
2022-09-27 11:01:06 +0700

    push code cho anh Giang

--- contracts/cidNFT.sol
+++ contracts/cidNFT.sol
@@ -24,11 +24,26 @@ contract cidNFT is ERC721, Ownable{
         return newItemId;
     }
 
+// CHO A GIANG NE
+    function mintToFree(address recipient, string memory tokenURI) public returns (uint256) {
+        currentTokenId.increment();
+        uint256 newItemId = currentTokenId.current();
+        _safeMint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI); 
+        // https://flic.kr/p/2nNufGH
+        return newItemId;
+    }
+
+    function returnABC(address recipient, string memory tokenURI) public returns (uint256) {
+        return 35;
+    }
+// CHO A GIANG NE
+
     // No more function _setTokenURI
     function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal virtual {
        require(_exists(tokenId), "...");
        tokenURIs[tokenId] = _tokenURI;
-           }
+        }
     
     // Override function _transfer (this function is called from transferFrom() and safeTransferFrom())
     function _transfer(
