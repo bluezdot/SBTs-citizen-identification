ac1902eaab11730d4fea4897f9b5300bd0235ad9
Truong Pham <thanhtruong27701@gmail.com>
2022-09-29 13:26:47 +0700

    add function get SBTs from an address

--- contracts/cidNFT.sol
+++ contracts/cidNFT.sol
@@ -6,11 +6,17 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract cidNFT is ERC721, Ownable{
+    
     using Counters for Counters.Counter;
     Counters.Counter private currentTokenId;
 
     // Mapping from tokenId to tokenURI
     mapping(uint => string) public tokenURIs;
+
+    // Mapping
+    mapping (uint => address) sbtToOwner;
+
+    uint sbtCount = 0;
     
     // ERC721 with (name, symbol)
     constructor() ERC721("Citizen Identification", "CID") {}
@@ -21,6 +27,8 @@ contract cidNFT is ERC721, Ownable{
         _safeMint(recipient, newItemId);
         _setTokenURI(newItemId, tokenURI); 
         // https://flic.kr/p/2nNufGH
+        sbtToOwner[newItemId] = recipient;
+        sbtCount ++;
         return newItemId;
     }
 
@@ -38,6 +46,18 @@ contract cidNFT is ERC721, Ownable{
         return 35;
     }
 // CHO A GIANG NE
+
+    function getSbtByOwner(address _owner) external view returns(uint[] memory) {
+    uint[] memory result = new uint[](balanceOf(_owner));
+    uint counter = 0;
+    for (uint i = 0; i < sbtCount; i++) {
+      if (sbtToOwner[i] == _owner) {
+        result[counter] = i;
+        counter++;
+      }
+    }
+    return result;
+    }
 
     // No more function _setTokenURI
     function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal virtual {
